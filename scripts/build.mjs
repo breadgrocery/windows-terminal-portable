5c5d70d8d12df25263df21de9c783ce405638646
breadgrocery <breadgrocery@gmail.com>
2025-11-16 21:58:58 +0800

    ci

---
+++ scripts/build.mjs
@@ -0,0 +1,19 @@
+import fs from "fs-extra";
+import path from "path";
+import { zip } from "compressing";
+
+const appsDir = path.resolve("src", "Apps");
+if (fs.pathExistsSync(appsDir) && fs.readdirSync(appsDir).length > 0) {
+  const source = path.resolve("dist", "Windows Terminal");
+  fs.copySync(path.resolve("src"), source, { dereference: true });
+
+  const dest = path.resolve("dist", "Windows Terminal.zip");
+
+  console.log(`Build: Compressing to ${dest}...`);
+
+  await zip.compressDir(source, dest, { compress: true, compressionLevel: 9 });
+
+  console.log("Build: Done.");
+} else {
+  console.log("Build: Step skipped.");
+}
